afe6c9140b61deffcb6c9f7ef2c35d31f96d583b
zhenhui.czh <zhenhui.czh@alibaba-inc.com>
2018-03-10 21:17:24 +0800

    suspress warnings

--- uac-service/src/main/java/com/zhenhui/demo/uac/service/controller/SignInController.java
+++ uac-service/src/main/java/com/zhenhui/demo/uac/service/controller/SignInController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unchecked")
 @RestController
 @RequestMapping("/user")
 public class SignInController {
